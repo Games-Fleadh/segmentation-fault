870537ff99cf0e9ac91e34a2e9f47d8531b51e26
ethanEverel123 <C22351646@mytudublin.ie>
2024-02-20 16:08:13 +0000

    This entire time it should have been working but it wasnt due to me not baking the Nav mesh. ALWAYS BAKE THE NAV MESH!!!!!!

--- scripts/chaser.gd
+++ scripts/chaser.gd
@@ -1,7 +1,5 @@
 extends CharacterBody3D
 
-
-
 var player = null
 
 const SPEED : float = 1.0
@@ -11,20 +9,19 @@ const SPEED : float = 1.0
 @onready var nav_agent = $NavigationAgent3D
 
 
-
 func _ready():
 	push_warning("player_path ::::: ", player_path)
-	player = get_node("/root/World/Player")
+	player = get_node("../../Player")
 	push_warning("player", player)
 
 func _process(_delta):
 	velocity = Vector3.ZERO
 	
 	nav_agent.set_target_position(player.global_position)
+	push_warning()
 	var next_nav_point  = nav_agent.get_next_path_position()
-	self.velocity = (player.global_position - self.global_position).normalized() * SPEED
+	velocity = (next_nav_point - self.global_position).normalized() * SPEED
+
 	push_warning(next_nav_point, self.global_position, velocity)
+
 	move_and_slide()
-	
-	
-		
