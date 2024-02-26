5d48d60a3a8f9ee397125f1bee344f1ecb94bc5d
Maryam <135457385+maryamalateye@users.noreply.github.com>
2024-02-26 18:02:51 +0000

    added ambient background sound

---
+++ game/zones/space_test/ambient.gd
@@ -0,0 +1,14 @@
+extends AudioStreamPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+#function that will play music when audio disk is read
+func _play_background() -> void: pass
