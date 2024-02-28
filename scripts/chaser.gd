09a9c5de2c3c6f69b4ab19edc9bb6b9222bf2f2c
ethanEverel123 <124163187+ethanEverel123@users.noreply.github.com>
2024-02-28 17:30:37 +0100

    Update chaser.gd
    My local repo wasnt working for some reason,
    detection range added

--- scripts/chaser.gd
+++ scripts/chaser.gd
@@ -15,13 +15,15 @@ func _ready():
 	push_warning("player", player)
 
 func _process(_delta):
-	velocity = Vector3.ZERO
+	var distance = (global_position - player.global_position)
 	
-	nav_agent.set_target_position(player.global_position)
-	push_warning()
-	var next_nav_point  = nav_agent.get_next_path_position()
-	velocity = (next_nav_point - self.global_position).normalized() * SPEED
+	if(distance.length() < enemy_attack_distance ):
+		velocity = Vector3.ZERO
+		
+		nav_agent.set_target_position(player.global_position)
+		var next_nav_point  = nav_agent.get_next_path_position()
+		velocity = (next_nav_point - self.global_position).normalized() * SPEED
 
-	push_warning(next_nav_point, self.global_position, velocity)
-
-	move_and_slide()
+		move_and_slide()
+	else:
+		pass
