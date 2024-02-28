ac452246384be6bbde8c06f65c7ae70b2539b0c0
derv <dervlaoflynn@gmail.com>
2024-02-28 17:14:15 +0000

    Balloon added as a child node to the camera. Need to research GUIs more as it does not run in XR.

---
+++ game/zones/space_test/XRCameraDialogue.gd
@@ -0,0 +1,12 @@
+extends XRCamera3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# Call the Intro Dialogue Script
+	#DialogueManager.show_dialogue_balloon(load("res://writing/Intro.dialogue"), "Intro")
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
