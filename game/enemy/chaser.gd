03b9ea14169d9112f0ac336b4d10ffbc12a9a8eb
CaptainComedy749 <jamie.lawlor2003@gmail.com>
2024-02-28 17:44:36 +0000

    Combat system implemented (oh my god).

--- game/enemy/chaser.gd
+++ game/enemy/chaser.gd
@@ -1,8 +1,11 @@
 extends CharacterBody3D
 
 var player = null
+var animator
+var rng = RandomNumberGenerator.new()
+var my_random_number = rng.randf_range(0, 2)
 
-const SPEED : float = 1.75
+const SPEED : float = 2.5
 
 @export var player_path : NodePath
 
@@ -11,8 +14,12 @@ const SPEED : float = 1.75
 
 func _ready():
 	push_warning("player_path ::::: ", player_path)
-	player = get_node("../../XROrigin3D")
+	player = get_node(player_path)
 	push_warning("player", player)
+
+	animator = get_node("AnimationPlayer")
+	await get_tree().create_timer(my_random_number).timeout
+	animator.play("enemyWalkLib/enemy_stagger")
 
 func _process(_delta):
 	velocity = Vector3.ZERO
@@ -25,9 +32,19 @@ func _process(_delta):
 	push_warning(next_nav_point, self.global_position, velocity)
 
 	move_and_slide()
-	
+	for i in get_slide_collision_count():
+		
+		var collision = get_slide_collision(i)
+		print("I collided with ", collision.get_collider().name)
+		
+		if collision.get_collider().name == "thefinnsword - Textures":
+			queue_free()
+		
+		
+
 	turn_face(player, _delta)
-	
+
+
 func turn_face(target, delta):
 	var rotation_speed = 3
 	var target_pos = target.global_transform.origin
@@ -39,3 +56,4 @@ func turn_face(target, delta):
 	var rot_length = target_rot - origin_rot
 	var rot_step = rot_length * rotation_speed * delta
 	rotation = origin_rot + rot_step
+	
