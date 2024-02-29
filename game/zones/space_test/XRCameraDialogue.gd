9dc4d4a13f505605caf5fcd2685b2329909ce44a
Derv <dervlaoflynn@gmail.com>
2024-02-29 20:22:35 +0000

    Progress on script for dialogue following the player camera

--- game/zones/space_test/XRCameraDialogue.gd
+++ game/zones/space_test/XRCameraDialogue.gd
@@ -4,14 +4,16 @@ var vr_camera # Reference to the VR camera or controller
 
 func _ready():
 	# Get reference to the VR camera or controller (adjust as necessary)
-	vr_camera = get_node("XROrigin3D/XRCamera3D")
+	vr_camera = get_node("/root/SpaceZone/XROrigin3D/XRCamera3D")
 
 	# Attach the dialogue balloon UI to the VR camera or controller
-	$DialogueBalloon.set_parent(vr_camera)
-	# Call the Intro Dialogue Script
+	# $Balloon.set_parent(vr_camera)
+	#Call the Intro Dialogue Script
 	#DialogueManager.show_dialogue_balloon(load("res://writing/Intro.dialogue"), "Intro")
+	# Attach the dialogue balloon UI to the VR camera or controller
+	# vr_camera.add_child($Balloon)
 
 func _process(delta):
 	# Update the position of the dialogue balloon UI to stay in front of the camera or controller
-	$DialogueBalloon.transform.origin = vr_camera.transform.origin + vr_camera.transform.basis.z * 2.0
+	$Balloon.transform.origin = vr_camera.transform.origin + vr_camera.transform.basis.z * 2.0
 
