207024b073d9145f9b058210bb02ac725d72885b
derv <dervlaoflynn@gmail.com>
2024-02-29 19:01:59 +0000

    Progress on camera node. Not working yet

--- game/zones/space_test/XRCameraDialogue.gd
+++ game/zones/space_test/XRCameraDialogue.gd
@@ -1,12 +1,17 @@
 extends XRCamera3D
 
+var vr_camera # Reference to the VR camera or controller
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
+	# Get reference to the VR camera or controller (adjust as necessary)
+	vr_camera = get_node("XROrigin3D/XRCamera3D")
+
+	# Attach the dialogue balloon UI to the VR camera or controller
+	$DialogueBalloon.set_parent(vr_camera)
 	# Call the Intro Dialogue Script
 	#DialogueManager.show_dialogue_balloon(load("res://writing/Intro.dialogue"), "Intro")
-	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	# Update the position of the dialogue balloon UI to stay in front of the camera or controller
+	$DialogueBalloon.transform.origin = vr_camera.transform.origin + vr_camera.transform.basis.z * 2.0
+
