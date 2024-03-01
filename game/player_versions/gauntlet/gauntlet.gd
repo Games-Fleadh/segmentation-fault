7ba82d5b2d27477c30bc8e0f2ee0167830e90562
CaptainComedy749 <jamie.lawlor2003@gmail.com>
2024-03-01 13:51:21 +0000

    Added ability for gauntlet to spawn sword.

--- game/player_versions/gauntlet/gauntlet.gd
+++ game/player_versions/gauntlet/gauntlet.gd
@@ -1,5 +1,4 @@
 extends Node3D
-
 @onready var _controller := XRHelpers.get_xr_controller(self)
 var buttonPressed = false
 var state = "State 1"
@@ -8,23 +7,29 @@ var colorAnimator
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	colorAnimator = get_node("AnimationPlayer")
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if _controller.is_button_pressed("ax_button") && buttonPressed == false && state == "State 1":
+	if _controller.is_button_pressed("by_button") && buttonPressed == false && state == "State 1":
 		print("Now in State 2")
 		state = "State 2"
 		buttonPressed = true
 		colorAnimator.play("gauntletLib/toTelekinesis")
+		$SwordSpawn.set_process(false)
+		$TelekinesisPoint.set_process(true)
 
-	if _controller.is_button_pressed("ax_button") && buttonPressed == false && state == "State 2":
+	if _controller.is_button_pressed("by_button") && buttonPressed == false && state == "State 2":
 		print("Now in State 1")
 		state = "State 1"
 		buttonPressed = true
 		colorAnimator.play("gauntletLib/toSword")
+		$TelekinesisPoint.set_process(false)
+		$SwordSpawn.set_process(true)
+		
 
-	if !_controller.is_button_pressed("ax_button"):
+	if !_controller.is_button_pressed("by_button"):
 		buttonPressed = false
 
 
