f205797c637784bf16652d0c847940abba2b8eda
Derv <dervlaoflynn@gmail.com>
2024-03-03 20:49:39 +0000

    Dialogic suffers from the same problems as the Dialogue Manager.

---
+++ game/zones/space_test/XRCamera3D.gd
@@ -0,0 +1,11 @@
+extends XRCamera3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Dialogic.start("Intro_Timeline")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
