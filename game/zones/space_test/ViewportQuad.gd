06b32d1d6df150d57977a09eac9979808c2a87bb
Derv <dervlaoflynn@gmail.com>
2024-03-03 15:22:41 +0000

    Dialogic added to try new dialogue plugin

---
+++ game/zones/space_test/ViewportQuad.gd
@@ -0,0 +1,11 @@
+extends MeshInstance3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#DialogueManager.show_dialogue_balloon(load("res://writing/Intro.dialogue"), "Intro")
+	pass
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
